f7334952a0c7446547fb53ea43b23dde4fe82758
Nguyễn Hoàng Anh <legendarycaptains@gmail.com>
2022-07-20 00:22:17 +0700

    add service foreground

--- app/src/main/java/com/example/recyclersample/service/MediaService.kt
+++ app/src/main/java/com/example/recyclersample/service/MediaService.kt
@@ -1,27 +1,75 @@
 package com.example.recyclersample.service
 
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.media.MediaPlayer
 import android.os.IBinder
-import android.provider.Settings
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import com.example.recyclersample.R
+import com.example.recyclersample.app.MyApplication
+import com.example.recyclersample.data.Song
+import com.example.recyclersample.flowerList.FlowersListActivity
 
 class MediaService : Service() {
     // Tạo object MediaPlayer
-    private lateinit var player : MediaPlayer
+    private lateinit var player: MediaPlayer
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // Tạo media player với default ringtone
-        player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI)
-        player.isLooping = true
-        player.start()
+        val bundle = intent?.extras
+
+        if (bundle != null) {
+            val song: Song = bundle.get("object song") as Song
+            playMusic(song)
+            sendNotification(song)
+        }
 
         // Return status of program
-        return START_STICKY
+        return START_NOT_STICKY
     }
 
     override fun onBind(p0: Intent?): IBinder? {
         return null
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        if (player != null) {
+            player.stop()
+        }
+    }
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    private fun sendNotification(song: Song) {
+        val intent = Intent(this, FlowersListActivity::class.java)
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val bitmap: Bitmap = BitmapFactory.decodeResource(resources, song.image)
+        val remoteView = RemoteViews(packageName, R.layout.notification)
+        remoteView.setTextViewText(R.id.tv_title, song.title)
+        remoteView.setTextViewText(R.id.tv_singer, song.singer)
+        remoteView.setImageViewBitmap(R.id.img_song, bitmap)
+        remoteView.setImageViewResource(R.id.btn_play, R.drawable.ic_vector_play_24dp)
+
+        val notification: Notification =
+            NotificationCompat.Builder(this, MyApplication.CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_vector_close)
+                .setContentIntent(pendingIntent)
+                .setCustomContentView(remoteView)
+                .setSound(null)
+                .build()
+
+        startForeground(101, notification)
+    }
+
+    private fun playMusic(song : Song) {
+        player = MediaPlayer.create(applicationContext, song.resource)
+        player.start()
+    }
 }
